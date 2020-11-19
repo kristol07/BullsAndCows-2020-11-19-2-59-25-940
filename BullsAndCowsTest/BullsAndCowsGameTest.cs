6b31be636dfdeac8d2cf5ecc53c95aa5f6bb8601
Lin Liu <LLiu41@slb.com>
2020-11-19 16:36:54 +0800

    Test: add more tests for 0A4B case

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -42,15 +42,17 @@ namespace BullsAndCowsTest
             Assert.Equal("4A0B", answer);
         }
 
-        [Fact]
-        public void Should_return_0A4B_when_all_digit_are_correct_but_random()
+        [Theory]
+        [InlineData("4 3 1 2")]
+        [InlineData("4 3 2 1")]
+        public void Should_return_0A4B_when_all_digit_correct_and_all_position_wrong(string guess)
         {
             //given
             var secretGenerator = new TestSecretGenerator();
             var game = new BullsAndCowsGame(secretGenerator);
 
             //when
-            string answer = game.Judge("4 3 2 1");
+            string answer = game.Judge(guess);
 
             //then
             Assert.Equal("0A4B", answer);
